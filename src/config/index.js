608156c8bf096239e6dbe787ada2d6c7ee9ddfa3
kusty <imkusty@gmail.com>
2018-05-06 21:59:45 +0800

    项目结构调整

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: wayne
+ * @Date: 2018-05-03 13:57:34
+ * @Last Modified by:   wayne
+ * @Last Modified time: 2018-05-03 13:57:34
+ */
+const conf = {
+  apiUrl: 'http://127.0.0.1:3900/'
+}
+export default conf
