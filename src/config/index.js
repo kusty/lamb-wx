41a57a2bda4c3bb65c9868e2426a9baf9d26d881
kusty <imkusty@gmail.com>
2018-05-24 14:00:23 +0800

    feature "mine页面"

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://12.0.0.12:3900'
+export const baseUrl = 'http://127.0.0.1:3900'
 export const imgUrl = 'http://img.kusty.live/'
 export const imgMode = {
   120: '?imageView2/1/w/240/h/240/q/100|imageslim'
